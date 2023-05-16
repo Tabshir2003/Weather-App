c366a4f7bdb90a0a2a6dbea94f5c6c18098666e6
nahfilaali <nahfila.ali05@myhunter.cuny.edu>
2023-05-15 23:12:17 -0400

    blue

--- index.js
+++ index.js
@@ -3,39 +3,55 @@ import fetch from 'node-fetch';
 import cors from 'cors';
 
 const app = express();
-app.use(express.static('public')); 
-app.use(cors()); // Add this line to enable CORS
+app.use(express.static('public'));
+app.use(cors());
 
 // This allows us to parse JSON data from the request body (if any).
-app.use(express.json())
+app.use(express.json());
 const PORT = 3001;
 
 // This is your root route. Just to used to ensure your ExpressJS server is up and running.
 app.get('/', (req, res) => {
   fetch('https://api.thecatapi.com/v1/images/search')
-    // This takes the raw response from the fetch promise and parses it to json data format.
+    // This takes the raw response from the fetch promise and parses it to JSON data format.
     .then(response => response.json())
-    // And then takes THAT promise and does things with the json data.
+    // And then takes THAT promise and does things with the JSON data.
     .then(data => {
       res.send(`
-        <h1>WeatherApp Ready To Go!</h1>
-        <img src='${data[0].url}'>
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <style>
+              body {
+                background-color: #71acef;
+                font-family: Arial, sans-serif;
+              }
+              h1 {
+                color: #10355f;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>WeatherApp Ready To Go!</h1>
+            <img src='${data[0].url}'>
+          </body>
+        </html>
       `);
     })
-})
+});
 
-// GET Request to get all the json data retrieved by city name.
-app.get("/weather/:city", (req, res) => {
+// GET Request to get all the JSON data retrieved by city name.
+app.get('/weather/:city', (req, res) => {
   const { city } = req.params;
-  const apiKey = "406a3468740e62be7410c1adc2da1810";
+  const apiKey = '406a3468740e62be7410c1adc2da1810';
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
   fetch(url)
-    .then((response) => response.json())
-    .then((data) => res.json(data))
-    .catch((error) => {
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(error => {
       console.log(error);
-      res.status(500).send("Error retrieving weather data");
+      res.status(500).send('Error retrieving weather data');
     });
 });
 
